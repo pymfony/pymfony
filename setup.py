4d8b31bc46cc78a3b7e68ef8fa5f2c4377733efa
alquerci <alquerci@email.com>
2013-02-17 11:18:25 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,61 +18,64 @@ realpathfile = os.path.realpath(os.path.dirname(__file__));
 os.chdir(realpathfile);
 
 f = open("README.md");
-long_description = f.read();
+long_description = "\n"+f.read();
 f.close();
 
-packages = [
-    'pymfony',
-    'pymfony.bundle',
-    'pymfony.bundle.framework',
-    'pymfony.bundle.framework.dependency',
-    'pymfony.component',
-    'pymfony.component.config',
-    'pymfony.component.config.definition',
-    'pymfony.component.console',
-    'pymfony.component.clikernel',
-    'pymfony.component.dependency',
-    'pymfony.component.dispatcher',
-    'pymfony.component.httpkernel',
-    'pymfony.component.kernel',
-    'pymfony.component.system',
-    'pymfony.component.system.py2',
-    'pymfony.component.system.py3',
-];
+def find_packages():
+    return [
+        'pymfony',
+        'pymfony.bundle',
+        'pymfony.bundle.framework',
+        'pymfony.bundle.framework.dependency',
+        'pymfony.component',
+        'pymfony.component.config',
+        'pymfony.component.config.definition',
+        'pymfony.component.console',
+        'pymfony.component.clikernel',
+        'pymfony.component.dependency',
+        'pymfony.component.dispatcher',
+        'pymfony.component.httpkernel',
+        'pymfony.component.kernel',
+        'pymfony.component.system',
+        'pymfony.component.system.py2',
+        'pymfony.component.system.py3',
+    ];
 
-resource_patterns = [
-    "Resources/config/*.*",
-];
-
-classifiers = [
-    'Framework :: Pymfony',
-    'Intended Audience :: Developers',
-    'License :: OSI Approved :: MIT',
-    'Operating System :: OS Independent',
-    'Programming Language :: Python',
-    'Programming Language :: Python 2.6',
-    'Programming Language :: Python 2.7',
-    'Programming Language :: Python 3.2',
-    'Programming Language :: Python 3.3',
-];
+def find_package_data():
+    return {
+        '': [
+            "Resources/config/*.*",
+        ]
+    };
 
 setup(
-    name='Pymfony',
-    packages=packages,
-    package_dir={'': 'src'},
-    package_data={'': resource_patterns},
+    name="pymfony",
     version="0.1.0",
-    long_description=long_description,
-    description="The Symfony2 PHP Framework with Python language",
-    license="MIT",
-    url="http://alquerci.github.com/pymfony",
-    download_url="https://github.com/alquerci/pymfony/tarball/master",
+    package_dir={'': 'src'},
+    packages=find_packages(),
+    package_data=find_package_data(),
     author="Alexandre Quercia",
     author_email="alquerci@email.com",
-    maintainer="Alexandre Quercia",
-    maintainer_email="alquerci@email.com",
-    platforms="all",
-    provides=packages,
-    keywords="framework",
-    classifiers=classifiers,
+    url="http://github.com/alquerci/pymfony",
+    description='An implementation of "Symfony2 PHP Framework" into Python',
+    long_description=long_description,
+    license="MIT",
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Environment :: Console",
+        "Environment :: Web Environment",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content"
+        "Topic :: Software Development :: Libraries :: PHP Classes",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+    ],
 );
