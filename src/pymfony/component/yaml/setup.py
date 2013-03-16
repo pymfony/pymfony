6181969bf4c3bee925a79fc70b3d815ec81cdb87
alquerci <alquerci@email.com>
2013-03-16 18:09:15 +0100

    [Yaml] Fix setup.py

--- src/pymfony/component/yaml/setup.py
+++ src/pymfony/component/yaml/setup.py
@@ -23,48 +23,27 @@ f.close();
 
 def find_packages():
     return [
-        'pymfony',
-        'pymfony.bundle',
-        'pymfony.bundle.framework_bundle',
-        'pymfony.bundle.framework_bundle.dependency',
-        'pymfony.component',
-        'pymfony.component.config',
-        'pymfony.component.config.definition',
-        'pymfony.component.console',
-        'pymfony.component.console_kernel',
-        'pymfony.component.console_routing',
-        'pymfony.component.dependency',
-        'pymfony.component.event_dispatcher',
-        'pymfony.component.httpkernel',
-        'pymfony.component.kernel',
-        'pymfony.component.system',
-        'pymfony.component.system.py2',
-        'pymfony.component.system.py2.minor6',
-        'pymfony.component.system.py3',
         'pymfony.component.yaml',
     ];
 
 def find_package_data():
     return {
-        '': [
-            "Resources/config/*.*",
-        ]
     };
 
 setup(
-    name="pymfony",
-    version="0.1.0",
-    package_dir={'': 'src'},
+    name="pymfony.yaml",
+    version="2.2.0b1",
+    package_dir={'pymfony.component.yaml': ''},
     packages=find_packages(),
     package_data=find_package_data(),
     author="Alexandre Quercia",
     author_email="alquerci@email.com",
-    url="http://github.com/alquerci/pymfony",
-    description='An implementation of "Symfony2 PHP Framework" into Python',
+    url="http://github.com/alquerci/pymfony-yaml",
+    description='Pymfony Yaml Component',
     long_description=long_description,
     license="MIT",
     classifiers=[
-        "Development Status :: 1 - Planning",
+        "Development Status :: 4 - Beta",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
@@ -74,11 +53,6 @@ setup(
         "Operating System :: OS Independent",
         "License :: OSI Approved :: MIT License",
         "Intended Audience :: Developers",
-        "Environment :: Console",
-        "Environment :: Web Environment",
-        "Topic :: Internet :: WWW/HTTP",
-        "Topic :: Internet :: WWW/HTTP :: Dynamic Content"
-        "Topic :: Software Development :: Libraries :: PHP Classes",
         "Topic :: Software Development :: Libraries :: Application Frameworks",
     ],
 );
